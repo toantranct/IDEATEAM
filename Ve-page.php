ca6320fa713e5c2c2b0ab82ed2395c0fef2e4a60
toantranct <mxhios@gmail>
2021-11-05 16:33:11 +0700

    .

---
+++ Ve-page.php
@@ -0,0 +1,52 @@
+<?php
+    include 'connect.php';
+    $sql  = "select * from Ve";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+
+    echo '
+        <div class="table-rep-plugin">
+        <div class="table-responsive" data-pattern="priority-columns">
+            <table id="tech-companies-1" class="table  table-striped">
+                <thead>
+                    <tr>
+                        <th data-priority="1">Mã Vé</th>
+                        <th data-priority="3">Ngày đặt</th>
+                        <th data-priority="1">Ghế</th>
+                        <th data-priority="3">Giá vé</th>
+                        <th data-priority="3">Giảm giá</th>
+                        <th data-priority="6">Giá vé thực</th>
+                        <th data-priority="6">Mã Khách mua</th>
+                        <th data-priority="6">Mã Nhân viên bán</th>
+                        <th data-priority="6">Mã Suất chiếu</th>
+                    </tr>
+                </thead>
+                <tbody>
+    ';
+    while ($row = $stmt->fetch()) {   
+    echo "
+                    <tr>
+                        <td>".$row['MaVe']."</td>
+                        <td>".$row['NgayDat']."</td>
+                        <td>".$row['Ghe']."</td>
+                        <td>".$row['GiaVe']."</td>
+                        <td>".$row['GiamGia']."</td>
+                        <td>".$row['GiaVeThuc']."</td>
+                        <td>".$row['MaK']."</td>
+                        <td>".$row['MaNV']."</td>
+                        <td>".$row['MaCTSC']."</td>
+                    </tr>
+                    
+              " ;
+    }
+
+
+    echo '
+        </tbody> </table> </div></div>';
+
+?>
+         
+       
--- connect.php
+++ connect.php
@@ -4,13 +4,25 @@
     $password = "123456";  
     $databaseName = "db_RAPCHIEUPHIM_BTL";  
 
+   
     try {
       $conn = new PDO("sqlsrv:server=$serverName;Database=$databaseName", $user, $password);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo "Connected successfully";
+      // echo "Connected successfully";
     } catch(PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
+      die("Connection failed: " . $e->getMessage());
     }
- 
+
+    // VD: Xử lý SELECT
+
+    // $sql  = "select * from Ve";
+    // $stmt = $conn->prepare($sql);
+    // $stmt->execute();
+
+    // $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    // while ($row = $stmt->fetch()) {   
+    //   // Gọi từng row một
+    //   print_r($row['MaVe']);
+    // }
 ?>
